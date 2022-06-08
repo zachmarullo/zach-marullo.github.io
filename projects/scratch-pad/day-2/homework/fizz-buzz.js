e554df69a55ab22e77ba4d5d831405215e5ffcac
zachmarullo <zachmarullo@gmail.com>
2022-06-08 02:11:13 +0000

    Fizzbuzz Completed

--- projects/scratch-pad/day-2/homework/fizz-buzz.js
+++ projects/scratch-pad/day-2/homework/fizz-buzz.js
@@ -12,9 +12,18 @@
 */
 function fizzBuzz() {
     // YOUR CODE GOES BELOW HERE //
-
-
-
+    var i = 1;
+    while( i <= 100){
+      if (i % 5 === 0 && i % 3 === 0){
+              console.log("FizzBuzz");
+    } else if (i % 5 === 0){
+        console.log("Buzz")
+    } else if (i % 3 === 0){
+        console.log("Fizz")
+    } else{
+      console.log(i);
+    }  i++;
+    }
     // YOUR CODE GOES ABOVE HERE //
 }
 
