d762589803fe741ea356ba2bcfeb101e0edf007b
zachmarullo <zachmarullo@gmail.com>
2022-06-13 21:05:25 +0000

    some of Animals work

---
+++ projects/matchy/data.js
@@ -0,0 +1,125 @@
+/**
+ * Part 1
+ *
+ * In this file, we're going to practice
+ * creating and accessing data structues.
+ *
+ * See the README for detailed instructions,
+ * and read every instruction carefully.
+ */
+
+//////////////////////////////////////////////////////////////////////
+// Step 1 - Object Creation //////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////
+
+var animal = {
+
+
+};
+
+animal.species = 'gorilla';
+
+animal['name'] = 'leeroy';
+
+animal.noises = [];
+
+console.log(animal);
+
+
+
+//////////////////////////////////////////////////////////////////////
+// Step 2 - Array Creation ///////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////
+var noises = [];
+
+noises[0] = 'AHHHHH';
+noises.push('roof roof');
+noises.unshift('bang bang');
+noises[3] = 'woof';
+
+//////////////////////////////////////////////////////////////////////
+// Step 3 - Combining Step 1 and 2 ///////////////////////////////////
+//////////////////////////////////////////////////////////////////////
+animal['noises'] = noises;
+animal.noises.push('boom boom');
+console.log(animal);
+/* *******************************************************************
+ * Step 4 - Review
+ *
+ * 1. What are the different ways you can access properties on objects?
+ *
+ * 2. What are the different ways of accessing elements on arrays?
+ *
+ * *******************************************************************
+ */
+
+/* *******************************************************************
+ * Step 5 - Take a Break!
+ *
+ * It's super important to give your brain and yourself
+ * a rest when you can! Grab a drink and have a think!
+ * For like 10 minutes, then, BACK TO WORK! :)
+ * *******************************************************************
+ */
+
+//////////////////////////////////////////////////////////////////////
+// Step 6 - A Collection of Animals //////////////////////////////////
+//////////////////////////////////////////////////////////////////////
+var animals = [];
+
+animals.push(animal);
+
+console.log(animals);
+
+var duck = {
+ species: 'duck', 
+ name: 'Jerome', 
+ noises: ['quack', 'honk', 'sneeze', 'woosh']
+};
+
+animals.push(duck);
+
+var horse = {
+  species: 'horse',
+  name: 'John',
+  noises: ['neigh', 'winny', 'horse noise']
+};
+
+animals.push(horse);
+
+var dog = {
+  species: 'dog',
+  name: 'Andy',
+  noises: ['woof', 'bark', 'roof']
+};
+
+animals.push(dog);
+
+
+
+
+//////////////////////////////////////////////////////////////////////
+// Step 7 - Making Friends ///////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////
+
+
+
+/**
+ * Nice work! You're done Part 1. Pat yourself on the back and
+ * move onto Part 2 in the file called "functions.js"
+ */
+
+
+
+//////////////////////////////////////////////////////////////////////
+// DON'T REMOVE THIS CODE ////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////
+var animal, noises, animals, friends, getRandom;
+if((typeof process !== 'undefined') &&
+   (typeof process.versions.node !== 'undefined')) {
+     module.exports.animal = animal || null;
+     module.exports.noises = noises || null;
+     module.exports.animals = animals || null;
+     module.exports.friends = friends || null;
+     module.exports.getRandom = getRandom || null;
+}
